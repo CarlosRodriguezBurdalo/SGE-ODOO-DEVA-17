28a2d4a2849e84ea95fd1b078f954a8f73173096
CarlosRodriguezBurdalo <crburdalo@gmail.com>
2025-02-10 09:47:16 +0100

    Introducimos con el odoo libreria y dos fichero de categoria y libro

---
+++ odoo/addons/libreria/models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class /mnt/extra-addons/libreria(models.Model):
+#     _name = '/mnt/extra-addons/libreria./mnt/extra-addons/libreria'
+#     _description = '/mnt/extra-addons/libreria./mnt/extra-addons/libreria'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
